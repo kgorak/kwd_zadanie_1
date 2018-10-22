8237d4b37f638861f65a1bb3dc58bc4b8c71f8e9
Kamil Górak <kamil.gorak@outlook.com>
2018-10-22 09:31:42 +0200

    Add predict method

--- knn/knn.py
+++ knn/knn.py
@@ -2,3 +2,6 @@ class kNN:
     def __init__(self, k, learning_data):
         self.k = k
         self.learning_data = learning_data
+
+    def predict(self, objects_to_classify):
+        return []
--- knn/tests/test_knn.py
+++ knn/tests/test_knn.py
@@ -6,3 +6,10 @@ import knn.knn as knn
 class kNNTest(unittest.TestCase):
     def test_constructor_accepts_arguments(self):
         knn.kNN(123, [1, 2, 3])
+
+    def test_predict(self):
+        solution = knn.kNN(123, [1, 2, 3])
+
+        result = solution.predict([])
+
+        self.assertIsInstance(result, list)
